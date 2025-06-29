82bfcddc6905c42ca28b8d3df6469a03e6bde869
asiman2344 <asimanm024@gmail.com>
2025-06-29 16:57:41 +0400

    uptaded

--- carrent/components/cardContainer.jsx
+++ carrent/components/cardContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './cardContainer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPerson, faGasPump, faCarSide, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import Car from '../assets/carImage.webp'
 import Card from './card'
 
@@ -9,14 +9,21 @@ function cardContainer() {
   return (
     <div className='cardContainer'>
       <div className="container">
-        <div className='card-title-container'>
+        <div className='card-title-container' data-aos="fade-up">
           <h1 className='card-title'>Featured Vehicles</h1>
           <p className='card-description'>Explore our selection of premium vehicles available for your next adventure.</p>
         </div>
-        <div className='card-wrapper'>
+        <div className='card-wrapper' data-aos="fade-up" data-aos-delay="100">
           <Card />
           <Card />
           <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className='card-button-container'>
+          <button className='card-button'>View All Cars<FontAwesomeIcon icon={faArrowRight} /></button>
         </div>
       </div>
     </div>
