97cd96f64d7a4789b93235ecc3dc7760bd3c8e44
asiman2344 <asimanm024@gmail.com>
2025-07-14 16:53:33 +0400

    ,

--- carrent/components/login.jsx
+++ carrent/components/login.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { logout } from '../features/slicer'
+import { logout, login } from '../features/slicer'
+import axios from 'axios'
 import './login.css'
 
 function Login() {
@@ -22,23 +23,47 @@ function Login() {
     }
   };
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const response = await axios.post('http://localhost:3000/login', { email, password });
+    console.log(response);
+    // if (response.status === 200) {
+    //   dispatch(login());
+    // }
+  }
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const response = await axios.post('http://localhost:3000/signup', { name, email, password });
+    console.log(response);
+    if (response.status === 201) {
+      setIsSignup(false);
+    }
+  }
+
   return (
     <div className={`login-container ${isLogin ? 'block' : 'hidden'}`} onClick={handleContainerClick}>
       <div className={`login-form ${isSignup ? 'z-2' : 'z-3'}`}>
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Login</h1>
-          <input type="text" placeholder='Email' />
-          <input type="password" placeholder='Password' />
+          <input name='email' type="text" placeholder='Email' />
+          <input name='password' type="password" placeholder='Password' />
           <button type='submit'>Login</button>
           <p>Don't have an account? <span onClick={()=>setIsSignup(true)}>Sign up</span></p>
         </form>
       </div>
       <div className='login-form signup-form z-2'>
-        <form>
+        <form onSubmit={handleSignup}>
           <h1>Sign up</h1>
-          <input type="text" placeholder='Name' />
-          <input type="email" placeholder='Email' />
-          <input type="password" placeholder='Password' />
+          <input name='name' type="text" placeholder='Name' />
+          <input name='email' type="email" placeholder='Email' />
+          <input name='password' type="password" placeholder='Password' />
           <button type='submit'>Sign up</button>
           <p>Already have an account? <span onClick={()=>setIsSignup(false)}>Login</span></p>
         </form>
