bb74aacf879151e36011484078c697429c033af2
asiman2344 <asimanm024@gmail.com>
2025-07-25 21:38:37 +0400

    logout system added

--- carrent/components/profile.jsx
+++ carrent/components/profile.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
-
+import Image from '../assets/customer.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencil } from '@fortawesome/free-solid-svg-icons'
+import './profile.css'
+import { useNavigate } from 'react-router-dom';//ececeb
 function profile() {
+  const navigate=useNavigate();
+
+  const Logout = () => {
+    localStorage.clear();
+    navigate('/');
+  }
+
   return (
-    <div>
-      profieleeee
+    <div className='profile-container'>
+      <div className="profile-card">
+        <div className='profile-card-image'>
+          <div className='profile-card-image-container'>
+            <img src={Image} alt='profile' />
+          </div>
+          <div className='profile-card-image-overlay'>
+            <FontAwesomeIcon icon={faPencil} />
+          </div>
+        </div>
+        <div className='profile-card-content'>
+          <h1><span>Name: </span>John Doe</h1>
+          <p><span>Mail: </span>john.doe@example.com</p>
+          <p><span>IsAdmin:</span> isAdmin</p>
+          <p><span>User created at: </span>120512</p>
+        </div>
+        <div className='logout-button'>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      </div>
     </div>
   )
 }
